494bf15cd4371ae6542151b07945a31831efca9c
zoon81 <zoon81@gmail.com>
2015-03-15 18:25:34 +0100

    Code formating done

--- python/CNCcontroller_KICAD.py
+++ python/CNCcontroller_KICAD.py
@@ -1,6 +1,9 @@
 import serial
 import time
+import sys
 
+print "Arg0:",sys.argv[1]
+fi = open(sys.argv[1], 'r')
 ser = serial.Serial()
 ser.port = "/dev/ttyUSB0"
 ser.xonxoff = False
@@ -11,43 +14,40 @@ ser.timeout = None
 ser.open()
 #get "hallo"
 print ser.readline(); 
-fi = open('nc', 'r')
 #first send delay data
 delayXY = 2
-delayZ =  3
-counter = 0
+delayZ =  5
+counter = 1
 ser.write( str(delayXY).zfill(3) )
 ser.write( str(delayZ).zfill(3) )
 print ser.readline();
 
 for f in fi:
-	if f[0] in['X']:		
+	if f[0] in['X']:
+#parsing gerber file		
 		xcoor = int( round(float(f[1:f.index('Y')]) / 28.93700787 ))
-		#print f.index('Y')
-		#print f[1:f.index('Y')]		
-		#print xcoor
-		ser.write( (str(xcoor).zfill(3)) )		
-		#print f.index('D')
-		#print f[f.index('Y')+1:f.index('D')]
 		if f[f.index('Y')+1] in['-']:
 			ycoor = int( round(float(f[(f.index('Y')+2):f.index('D')]) / 28.93700787 ))
 		else:
 			ycoor = 0
-		
-		#print ycoor
-		ser.write( (str(ycoor).zfill(3)) )
 		zcoor = f[f.index('D')+2]
-		#print zcoor
-		ser.write( str(zcoor) )
+		print counter,". ",f
 		if f[f.index('Y')+1] in['-']:
 			print ((str(float(f[1:f.index('Y')]) / 28.93700787 ))+' '+ (str(float(f[(f.index('Y')+2):f.index('D')]) / 28.93700787 )))
 		else:
 			print ((str(float(f[1:f.index('Y')]) / 28.93700787 ))+' '+ (str(float(f[(f.index('Y')+1):f.index('D')]) / 28.93700787 )))
 		print ((str(xcoor).zfill(3)) +' '+ (str(ycoor).zfill(3)) +' '+ str(zcoor))
-		print("[Plotter] I go to:")
-		print ser.readline();
-		print("[Plotter] Sent:")
-		print ser.readline();
+		
+#sending data 
+		ser.write( (str(xcoor).zfill(3)) )
+		ser.write( (str(ycoor).zfill(3)) )
+		ser.write( str(zcoor) )
+#get feedback
+		print("[Plotter] I go to:",ser.readline() )
+		print("[Plotter] Sent:",ser.readline() )
+		counter = counter + 1
+		if(zcoor in['0']):
+			var = raw_input("Press enter to continue!")
 
 
 ser.write( (str(0).zfill(3)) )
